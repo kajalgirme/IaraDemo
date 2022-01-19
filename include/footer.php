4508d43ea91db6422b3d154b671a57393646c464
kajalgirme <girmekkajal@gmail.com>
2022-01-19 19:16:55 +0530

    initial commit

---
+++ include/footer.php
@@ -0,0 +1,70 @@
+<footer>
+	<div class="footer-main footer-type-v1">
+		<div class="footer-inside">
+			<div class="container">
+				<div class="footer-inner">
+					<div class="row">
+						<div class="col-xs-12 col-sm-4 col-md-2">
+							<div class="widget_text widget widget_custom_html">
+								<div class="textwidget custom-html-widget">
+								   <img src="images/footer-logo.png">
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-xs-12 col-sm-4 col-md-4">
+							<div class="widget_text widget widget_custom_html">
+								<div class="textwidget custom-html-widget">
+									<div class="widget-contact-info">
+										<div class="widget-contact-item">	
+											
+											<p class="widget-contact-value"><a href="tel:0483 101 410">0483 101 410</a> |, <a href="mailto:hello@iarasupport.com.au">hello@iarasupport.com.au</a>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-4 col-md-2" style="padding-top: 20px;">
+							<div class="widget_text widget widget_custom_html">
+								<div class="textwidget custom-html-widget">
+								   <img src="images/tag-registered-provider_2020_white-mono.png">
+								</div>
+							</div>
+						</div>
+					    <div class="col-xs-12 col-sm-4 col-md-2">
+							<div class="widget_text widget widget_custom_html">
+								<div class="textwidget custom-html-widget">
+								   <img src="images/heart-logo.png" style="    height: 100px;">
+								</div>
+							</div>
+						</div>
+					    <div class="col-xs-12 col-sm-4 col-md-2">
+							<div class="widget_text widget widget_custom_html">
+								<div class="textwidget custom-html-widget">
+								  <a href="#" target="_blank"> <i class="fa fa fa-facebook" style="font-size:30px;background-color: grey;color:#ddd;padding: 20px;border-radius: 50%;"></i>
+								</div>
+							</div>
+						</div>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
+</div>	
+<a href="javascript:void(0);" class="top-arrow"><i class="fa fa-angle-up"></i></a>
+
+<div class="modal fade get_quote_model" id="get-quote-model" tabindex="-1" role="dialog" aria-labelledby="getmodalLabel">
+
+</div>
+<script type='text/javascript' src='js/wp-polyfill.min.js' id='wp-polyfill-js'></script>
+<script type='text/javascript' src='js/bootstrap.min.js' id=''></script>
+
+
+<script src="js/622279b1e9bbcb6e6c4a257d67d4e026.js" data-minify="1" defer></script>
+</body>
+
+</html>
---
+++ include/header.php
@@ -0,0 +1,141 @@
+<link rel="stylesheet" href="" />
+<link rel="stylesheet" href="css/style.css" media="all" data-minify="1" />
+<link rel="stylesheet" href="css/bootstrap.min.css" media="all" data-minify="1" />
+<meta name='robots' content='max-image-preview:large' />
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link href='https://fonts.gstatic.com' crossorigin rel='preconnect' />
+<link rel="alternate" type="application/rss+xml" title="" href="" />
+<link rel="alternate" type="application/rss+xml" title="" href="" />
+<style type="text/css">
+	img.wp-smiley,
+	img.emoji {
+		display: inline !important;
+		border: none !important;
+		box-shadow: none !important;
+		height: 1em !important;
+		width: 1em !important;
+		margin: 0 .07em !important;
+		vertical-align: -0.1em !important;
+		background: none !important;
+		padding: 0 !important;
+	}
+</style>
+<style id='rs-plugin-settings-inline-css' type='text/css'>
+	#rs-demo-id {}
+</style>
+<link rel='stylesheet' id='woocommerce-smallscreen-css' href='css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)' />
+<style id='woocommerce-inline-inline-css' type='text/css'>
+	.woocommerce form .form-row .required { visibility: visible; }
+</style>
+<style id='construction-style-inline-css' type='text/css'>
+	.header-menu-inner .menu-wrp ul li a, .top-address .top-address-inner a, .top-address .top-address-inner p, .top-address .top-address-inner p a, .top-links a, .mean-container .mean-nav ul li a, .woocommerce-cart-dropdown-item ul li a, .woocommerce-cart-dropdown-item ul li, .header-cart-area:hover .woocommerce-cart-dropdown-item, .explore-top-left, .explore-get a, .explore-timing-right span, .explore-menu, .tag-line-small, .tag-line-large, .counter-expand-left h3 {
+					font-family: Open Sans;
+					font-size: 16px;
+					font-weight: 400;
+					line-height: 25px;
+					text-transform: none;
+					text-align: ;
+					color: #fff;
+				}
+			
+				.menu-wrp ul li .submenu li a:hover {
+					background: #313134 none repeat scroll 0 0;
+				}
+			
+				.mean-container .mean-nav ul li a {
+					color: #ffffff !important;
+				}
+				.header-menu
+					color: #ffffff !important;
+					background-color: #204f4b !important; 
+				}
+			
+				.copyright-main {
+					border-top: 1px solid #212124;
+				}            
+			
+				.special-bottom-footer {
+					border-top: 1px solid #212124;
+				}
+</style>
+<script type='text/javascript' src='js/jquery.min.js' id='jquery-core-js' defer></script>
+<link rel="https://api.w.org/" href="" />
+<link rel="alternate" type="application/json" href="" />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="" />
+<meta name="generator" content="WordPress 5.8.1" />
+<meta name="generator" content="WooCommerce 5.7.0" />
+<link rel="canonical" href="" />
+<link rel='shortlink' href='' />
+<link rel="preload" as="font" href="fonts/fontawesome-webfont.woff2" crossorigin>
+<link rel="preload" as="font" href="fonts/revicons.woff" crossorigin>
+
+<link rel="shortcut icon" href="images/support-icon.png">
+<link rel="apple-touch-icon-precomposed" href="images/support-icon.png">
+<link rel="apple-touch-icon-precomposed" href="images/support-icon.png">
+<link rel="apple-touch-icon-precomposed" href="images/support-icon.png">
+<noscript>
+	<style>
+		.woocommerce-product-gallery{ opacity: 1 !important; }
+	</style>
+</noscript>
+<meta name="generator" content="" />
+<meta name="generator" content="Powered by Slider Revolution 6.1.5 - responsive, Mobile-Friendly Slider Plugin for WordPress with comfortable drag and drop interface." />
+<script type="text/javascript">
+	function setREVStartSize(t){try{var h,e=document.getElementById(t.c).parentNode.offsetWidth;if(e=0===e||isNaN(e)?window.innerWidth:e,t.tabw=void 0===t.tabw?0:parseInt(t.tabw),t.thumbw=void 0===t.thumbw?0:parseInt(t.thumbw),t.tabh=void 0===t.tabh?0:parseInt(t.tabh),t.thumbh=void 0===t.thumbh?0:parseInt(t.thumbh),t.tabhide=void 0===t.tabhide?0:parseInt(t.tabhide),t.thumbhide=void 0===t.thumbhide?0:parseInt(t.thumbhide),t.mh=void 0===t.mh||""==t.mh||"auto"===t.mh?0:parseInt(t.mh,0),"fullscreen"===t.layout||"fullscreen"===t.l)h=Math.max(t.mh,window.innerHeight);else{for(var i in t.gw=Array.isArray(t.gw)?t.gw:[t.gw],t.rl)void 0!==t.gw[i]&&0!==t.gw[i]||(t.gw[i]=t.gw[i-1]);for(var i in t.gh=void 0===t.el||""===t.el||Array.isArray(t.el)&&0==t.el.length?t.gh:t.el,t.gh=Array.isArray(t.gh)?t.gh:[t.gh],t.rl)void 0!==t.gh[i]&&0!==t.gh[i]||(t.gh[i]=t.gh[i-1]);var r,a=new Array(t.rl.length),n=0;for(var i in t.tabw=t.tabhide>=e?0:t.tabw,t.thumbw=t.thumbhide>=e?0:t.thumbw,t.tabh=t.tabhide>=e?0:t.tabh,t.thumbh=t.thumbhide>=e?0:t.thumbh,t.rl)a[i]=t.rl[i]<window.innerWidth?0:t.rl[i];for(var i in r=a[0],a)r>a[i]&&0<a[i]&&(r=a[i],n=i);var d=e>t.gw[n]+t.tabw+t.thumbw?1:(e-(t.tabw+t.thumbw))/t.gw[n];h=t.gh[n]*d+(t.tabh+t.thumbh)}void 0===window.rs_init_css&&(window.rs_init_css=document.head.appendChild(document.createElement("style"))),document.getElementById(t.c).height=h,window.rs_init_css.innerHTML+="#"+t.c+"_wrapper { height: "+h+"px }"}catch(t){console.log("Failure at Presize of Slider:"+t)}};
+</script>
+<link rel='stylesheet' id='woocommerce-smallscreen-css' href='css/styles.css' type='text/css' media='only screen and (max-width: 768px)' />
+<style type="text/css" data-type="vc_shortcodes-custom-css">
+	.vc_custom_1536725085944{background-color: #f5f5f5 !important;}.vc_custom_1577704587434{background-color: #f5f5f5 !important;}
+	.vc_custom_1536725129313{padding-top: 80px !important;padding-bottom: 80px !important;background-color: #204f4b !important;}
+	.vc_custom_1577704686938{background-color: #f5f5f5 !important;}.vc_custom_1577704806538{background-color: #f5f5f5 !important;}.vc_custom_1577704025731{padding-top: 0px !important;}.vc_custom_1577704172967{padding-top: 0px !important;}.vc_custom_1577704101448{margin-bottom: 0px !important;}.vc_custom_1489651620064{padding-top: 0px !important;}.vc_custom_1489651765963{padding-top: 55px !important;padding-bottom: 55px !important;}.vc_custom_1577704245562{padding-top: 0px !important;}.vc_custom_1577704348170{padding-top: 0px !important;}.vc_custom_1577704424503{padding-top: 0px !important;}.vc_custom_1578133523835{padding-top: 0px !important;}.vc_custom_1578133528590{padding-top: 0px !important;}.vc_custom_1578133533954{padding-top: 0px !important;}.vc_custom_1578133539430{padding-top: 0px !important;}.vc_custom_1578133544591{padding-top: 0px !important;}.vc_custom_1578133549250{padding-top: 0px !important;}.vc_custom_1577704445466{padding-top: 0px !important;}.vc_custom_1577704493922{padding-top: 0px !important;}.vc_custom_1577704529698{padding-top: 0px !important;}.vc_custom_1577704965427{padding-top: 0px !important;}.vc_custom_1577704608650{padding-top: 0px !important;}.vc_custom_1577705116494{padding-top: 0px !important;}.vc_custom_1577705079008{padding-top: 0px !important;}.vc_custom_1577704665409{padding-top: 0px !important;}.vc_custom_1577705134513{padding-top: 0px !important;}.vc_custom_1577705057647{margin-bottom: 0px !important;}.vc_custom_1577704844560{padding-top: 0px !important;}.vc_custom_1577705205259{padding-top: 0px !important;}.vc_custom_1577704840686{padding-top: 0px !important;}.vc_custom_1577705440611{padding-top: 0px !important;}.vc_custom_1577704836745{padding-top: 0px !important;}.vc_custom_1577705248733{padding-top: 0px !important;}.vc_custom_1577705257609{padding-top: 0px !important;}
+</style>
+<noscript>
+	<style type="text/css">
+		.wpb_animate_when_almost_visible { opacity: 1; }
+		
+	</style>
+</noscript>
+</head>
+
+<body class="page-template page-template-templates page-template-vc-page-template page-template-templatesvc-page-template-php page page-id-332 theme-construction woocommerce-no-js wpb-js-composer js-comp-ver-5.5.2 vc_responsive">
+<div class="wrapper" id="wrapper">
+	<header>
+		<div class="header-main header-slot sticky">
+		
+			<div class="header-menu ">
+				<div class="container">
+					<div class="header-menu-inner">
+						<div class="logo">
+							<a href="#">
+								<img src="images/logo.png" alt="Logo">
+							</a>
+						</div>
+						<div class="menu-wrp">
+							<nav>
+								<ul>
+									<li id="menu-item-1209" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1209">
+										<a href="index.php">Home</a>
+									</li>
+									<li id="menu-item-1343" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1209">
+										<a href="#">About Us</a>
+									</li>
+									<li id="menu-item-953" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-953">
+										<a href="#">Our Services</a>
+									</li>
+								   
+								
+									<li id="menu-item-953" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-953">
+										<a href="#">Our Team</a>
+									</li>
+									
+									<li id="menu-item-639" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-639">
+										<a href="#">Latest News</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		
